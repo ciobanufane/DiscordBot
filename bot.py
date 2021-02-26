b6ce51ba50eeff59edaa302abea0611461fd4c37
ciobanufane <ciobanufane17@users.noreply.github.com>
2021-02-25 18:26:25 -0800

    Added commands to add/delete text channels

--- bot.py
+++ bot.py
@@ -19,18 +19,39 @@ async def on_ready():
 
 @bot.command(name='echo')
 async def echo(ctx, *args):
-    test_channel = discord.utils.get(ctx.guild.channels, id=TEST)
-    await ctx.send(" ".join(args))
-
-
-@bot.command(name='test')
-async def test(ctx):
-    await ctx.send("test")
+    if str(ctx.channel.id) == TEST:
+        await ctx.send(" ".join(args), delete_after=15)
 
 
 @bot.command(name="ntc")
-async def new_text_channel(ctx):
-    pass
+@commands.has_guild_permissions(manage_channels=True)
+async def new_text_channel(ctx, channel_name, cat="Text Channels"):
+
+    exist_channel = discord.utils.get(ctx.guild.channels, name=channel_name)
+    exist_cat = discord.utils.get(ctx.guild.categories, name=cat)
+
+    if exist_channel:
+        await ctx.send(f"{channel_name} already exists; enter another channel name")
+    elif not exist_cat:
+        await ctx.send(f"No specified category for the text channel")
+    else:
+        await ctx.guild.create_text_channel(channel_name, category=exist_cat)
+
+
+@bot.command(name="dtc")
+@commands.has_guild_permissions(manage_channels=True)
+async def delete_text_channel(ctx, channel_name, category_name):
+    exist_cat = discord.utils.get(ctx.guild.categories, name=category_name)
+    if not exist_cat:
+        await ctx.send(f"{category_name} does not exist")
+        return
+
+    exist_channel = discord.utils.get(exist_cat.channels, name=channel_name)
+    if not exist_channel:
+        await ctx.send(f"{channel_name} does not exist under {exist_cat}")
+        return
+
+    await exist_channel.delete()
 
 
 @bot.command(name="nvc")
@@ -42,5 +63,10 @@ async def new_voice_channel(ctx):
 async def create_role(ctx):
     pass
 
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, discord.Forbidden):
+        await ctx.send("You do not have proper permissions", delete_after=15)
 
 bot.run(TOKEN)
